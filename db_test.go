7baaaf9dffb9ecfe8d79dcb0b6a03e3be9f3d45e
Alden <ygq13572549874@163.com>
2024-12-07 19:53:59 +0800

    Update db_test.go
    Signed-off-by: Alden <ygq13572549874@163.com>

--- db_test.go
+++ db_test.go
@@ -3,8 +3,6 @@ import (
     "testing"
     "fmt"
     "encoding/json"
-   "gorm.io/driver/mysql"
-   "gorm.io/gorm"
 )
 type Users struct {
     Id uint `gorm:"column:id;PRIMARY_KEY;type:int(10)"`
@@ -13,28 +11,45 @@ type Users struct {
     Status int `gorm:"column:status;type:int(10)";default:1`
 }
 func Test_InitDb(t *testing.T) {
-    dsn := "root:**********@tcp(127.0.0.1:3306)/user_info?charset=utf8&parseTime=True&loc=Local"
-    db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+    var conf *GormConfig  = &GormConfig{}
+    conf.User = "root"
+    conf.Passwd = "qiang19940114**"
+    conf.Host = "127.0.0.1"
+    conf.Port = 3306
+    conf.Dbcharset = "utf8"
+    conf.MaxIdleConns = 10
+    conf.MaxOpenConns = 100
+    conf.MaxConnLifeTime = 600
+    conf.DBName = "user_info"
+    dbclient,err := NewDBOperation(conf)
     if err != nil {
         fmt.Printf("connect mysql failed:%v\n",err)
         return
     }
-    if db != nil {
+    if dbclient != nil {
         fmt.Println("Database connected successfully")
     }
 }
 
 func Test_QueryRowGorm(t *testing.T) {
-    dsn := "root:**********@tcp(127.0.0.1:3306)/user_info?charset=utf8&parseTime=True&loc=Local"
-    db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-    if err != nil {
-        fmt.Printf("connect mysql failed:%v\n",err)
-        return
-    }
-    if db != nil {
-        fmt.Println("Database connected successfully")
-    }
-    dbclient := NewDBOperation(db)
+     var conf *GormConfig  = &GormConfig{}
+     conf.User = "root"
+     conf.Passwd = "qiang19940114**"
+     conf.Host = "127.0.0.1"
+     conf.Port = 3306
+     conf.Dbcharset = "utf8"
+     conf.MaxIdleConns = 10
+     conf.MaxOpenConns = 100
+     conf.MaxConnLifeTime = 600
+     conf.DBName = "user_info"
+     dbclient,err := NewDBOperation(conf)
+     if err != nil {
+         fmt.Printf("connect mysql failed:%v\n",err)
+         return
+     }
+     if dbclient != nil {
+         fmt.Println("Database connected successfully")
+     }
     var user *Users = &Users{}
    result,err := dbclient.QueryRow("users",user,"en_name = ? and status = ?","mingyu","2")
    if err != nil {
@@ -46,16 +61,24 @@ func Test_QueryRowGorm(t *testing.T) {
    fmt.Printf("output:%v\n",string(re))
 }
 func Test_QueryListGorm(t *testing.T) {
-    dsn := "root:**********@tcp(127.0.0.1:3306)/user_info?charset=utf8&parseTime=True&loc=Local"
-    db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-    if err != nil {
-        fmt.Printf("connect mysql failed:%v\n",err)
-        return
-    }
-    if db != nil {
-        fmt.Println("Database connected successfully")
-    }
-    dbclient := NewDBOperation(db)
+   var conf *GormConfig  = &GormConfig{}
+   conf.User = "root"
+   conf.Passwd = "qiang19940114**"
+   conf.Host = "127.0.0.1"
+   conf.Port = 3306
+   conf.Dbcharset = "utf8"
+   conf.MaxIdleConns = 10
+   conf.MaxOpenConns = 100
+   conf.MaxConnLifeTime = 600
+   conf.DBName = "user_info"
+   dbclient,err := NewDBOperation(conf)
+   if err != nil {
+       fmt.Printf("connect mysql failed:%v\n",err)
+       return
+   }
+   if dbclient != nil {
+       fmt.Println("Database connected successfully")
+   }
    var userlist []*Users = make([]*Users,0)
 
    var page int = 1
@@ -71,17 +94,24 @@ func Test_QueryListGorm(t *testing.T) {
 }
 
 func Test_InsertSingleRow(t *testing.T) {
-    dsn := "root:**********@tcp(127.0.0.1:3306)/user_info?charset=utf8&parseTime=True&loc=Local"
-    db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-    if err != nil {
-        fmt.Printf("connect mysql failed:%v\n",err)
-        return
-    }
-    if db != nil {
-        fmt.Println("Database connected successfully")
-    }
-    dbclient := NewDBOperation(db)
-
+   var conf *GormConfig  = &GormConfig{}
+   conf.User = "root"
+   conf.Passwd = "qiang19940114**"
+   conf.Host = "127.0.0.1"
+   conf.Port = 3306
+   conf.Dbcharset = "utf8"
+   conf.MaxIdleConns = 10
+   conf.MaxOpenConns = 100
+   conf.MaxConnLifeTime = 600
+   conf.DBName = "user_info"
+   dbclient,err := NewDBOperation(conf)
+   if err != nil {
+       fmt.Printf("connect mysql failed:%v\n",err)
+       return
+   }
+   if dbclient != nil {
+       fmt.Println("Database connected successfully")
+   }
     var user *Users = &Users{}
     user.EnName = "alden"
     user.Password = "alden"
@@ -95,16 +125,24 @@ func Test_InsertSingleRow(t *testing.T) {
 }
 
 func Test_InsertMutilRow(t *testing.T) {
-    dsn := "root:**********@tcp(127.0.0.1:3306)/user_info?charset=utf8&parseTime=True&loc=Local"
-    db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-    if err != nil {
-        fmt.Printf("connect mysql failed:%v\n",err)
-        return
-    }
-    if db != nil {
-        fmt.Println("Database connected successfully")
-    }
-    dbclient := NewDBOperation(db)
+   var conf *GormConfig  = &GormConfig{}
+   conf.User = "root"
+   conf.Passwd = "qiang19940114**"
+   conf.Host = "127.0.0.1"
+   conf.Port = 3306
+   conf.Dbcharset = "utf8"
+   conf.MaxIdleConns = 10
+   conf.MaxOpenConns = 100
+   conf.MaxConnLifeTime = 600
+   conf.DBName = "user_info"
+   dbclient,err := NewDBOperation(conf)
+   if err != nil {
+       fmt.Printf("connect mysql failed:%v\n",err)
+       return
+   }
+   if dbclient != nil {
+       fmt.Println("Database connected successfully")
+   }
     var users []*Users = make([]*Users,0)
     var user1 *Users = &Users{}
     var user2 *Users = &Users{}
@@ -125,16 +163,24 @@ func Test_InsertMutilRow(t *testing.T) {
 }
 
 func Test_DeleteRowGorm(t *testing.T) {
-    dsn := "root:**********@tcp(127.0.0.1:3306)/user_info?charset=utf8&parseTime=True&loc=Local"
-    db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-    if err != nil {
-        fmt.Printf("connect mysql failed:%v\n",err)
-        return
-    }
-    if db != nil {
-        fmt.Println("Database connected successfully")
-    }
-   dbclient := NewDBOperation(db)
+   var conf *GormConfig  = &GormConfig{}
+   conf.User = "root"
+   conf.Passwd = "qiang19940114**"
+   conf.Host = "127.0.0.1"
+   conf.Port = 3306
+   conf.Dbcharset = "utf8"
+   conf.MaxIdleConns = 10
+   conf.MaxOpenConns = 100
+   conf.MaxConnLifeTime = 600
+   conf.DBName = "user_info"
+   dbclient,err := NewDBOperation(conf)
+   if err != nil {
+       fmt.Printf("connect mysql failed:%v\n",err)
+       return
+   }
+   if dbclient != nil {
+       fmt.Println("Database connected successfully")
+   }
    var user *Users = &Users{}
 
    rows,err := dbclient.DeleteRow("users",user,"en_name = ?","alden")
@@ -147,16 +193,24 @@ func Test_DeleteRowGorm(t *testing.T) {
 }
 
 func Test_UpdateRow(t *testing.T) {
-    dsn := "root:**********@tcp(127.0.0.1:3306)/user_info?charset=utf8&parseTime=True&loc=Local"
-    db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-    if err != nil {
-        fmt.Printf("connect mysql failed:%v\n",err)
-        return
-    }
-    if db != nil {
-        fmt.Println("Database connected successfully")
-    }
-   dbclient := NewDBOperation(db)
+   var conf *GormConfig  = &GormConfig{}
+   conf.User = "root"
+   conf.Passwd = "qiang19940114**"
+   conf.Host = "127.0.0.1"
+   conf.Port = 3306
+   conf.Dbcharset = "utf8"
+   conf.MaxIdleConns = 10
+   conf.MaxOpenConns = 100
+   conf.MaxConnLifeTime = 600
+   conf.DBName = "user_info"
+   dbclient,err := NewDBOperation(conf)
+   if err != nil {
+       fmt.Printf("connect mysql failed:%v\n",err)
+       return
+   }
+   if dbclient != nil {
+       fmt.Println("Database connected successfully")
+   }
    var user *Users = &Users{}
    user.Status = 2
    rows,err := dbclient.UpdateRow("users",user,"en_name = ?","summer")
