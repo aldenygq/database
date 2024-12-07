07ef171e8133ec5097c8fa19fc57a862cace3b71
Alden <ygq13572549874@163.com>
2024-12-07 16:46:17 +0800

    Create test.sql
    Signed-off-by: Alden <ygq13572549874@163.com>

---
+++ test.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `users` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `en_name` varchar(256) NOT NULL DEFAULT '' COMMENT '英文名称',
+  `password` varchar(256) NOT NULL DEFAULT '' COMMENT '密码',
+  `status` int(11) NOT NULL DEFAULT '1' COMMENT '用户状态',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
